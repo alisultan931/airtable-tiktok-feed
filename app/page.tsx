c85e16c197ef5da45c9271617f241ae9d9b8a528
alisultan931 <alisultan931@gmail.com>
2026-02-24 19:32:55 +0500

    Performance Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,18 @@ import VideoFeed from "./components/VideoFeed";
 
 async function getVideos() {
   const res = await fetch(
-    `https://api.airtable.com/v0/${process.env.BASE_ID}/${process.env.TABLE_NAME}`,
+    `https://api.airtable.com/v0/${process.env.BASE_ID}/${process.env.TABLE_NAME}?view=API%20View`,
     {
       headers: {
         Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`,
       },
-      cache: "no-store",
+      next: { revalidate: 180 }, // cache for 3 mins
     }
   );
 
   const data = await res.json();
 
-  // Sort newest first
-  const sortedRecords = data.records.sort(
-    (a: any, b: any) =>
-      new Date(b.fields["Date Added"]).getTime() -
-      new Date(a.fields["Date Added"]).getTime()
-  );
-
-  return sortedRecords;
+  return data.records;
 }
 
 export default async function Home() {
